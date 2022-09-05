97b6447b62d9e850468b78e869484438eeed24b4
HudsonHawk88 <toth.gergo88@hotmail.com>
2022-09-05 18:02:10 +0200

    fix(app): fix static

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { hydrateRoot } from 'react-dom/client';
+import { hydrateRoot, createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 
@@ -8,9 +8,20 @@ import App from './App';
 const history = createBrowserHistory();
 const container = document.getElementById('root');
 
-const root = hydrateRoot(
-    container,
-    <Router>
-        <App serverData={window.__INITIAL_DATA__} history={history} />
-    </Router>
-);
+const isBrowser = __isBrowser__;
+
+if (isBrowser) {
+    const root = createRoot(container);
+    root.render(
+        <Router>
+            <App serverData={window.__INITIAL_DATA__} history={history} />
+        </Router>
+    );
+} else {
+    const root = hydrateRoot(
+        container,
+        <Router>
+            <App serverData={window.__INITIAL_DATA__} history={history} />
+        </Router>
+    );
+}
