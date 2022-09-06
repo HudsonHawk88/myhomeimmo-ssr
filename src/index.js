e3a48db6555f0e03f6ecfb79bdc45d4c598c955b
HudsonHawk88 <toth.gergo88@hotmail.com>
2022-09-06 03:38:01 +0200

    fix(app): fix og:image meta tag

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { hydrateRoot, createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
