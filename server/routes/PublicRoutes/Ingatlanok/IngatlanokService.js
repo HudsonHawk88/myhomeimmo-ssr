d61b6299625d969f7ff7a108895d5aa5df42f466
HudsonHawk88 <toth.gergo88@hotmail.com>
2022-11-12 19:15:28 +0100

    fix(App): kepmeret csokkentes

--- server/routes/PublicRoutes/Ingatlanok/IngatlanokService.js
+++ server/routes/PublicRoutes/Ingatlanok/IngatlanokService.js
@@ -235,7 +235,11 @@ router.get('/javitas', async (req, res) => {
         ress = result.map((ing) => {
             ing = getJSONfromLongtext(ing, 'toBool');
             ing.kepek.map((item) => {
-                item.src = `http://inftechsol.hu:8460/static/images/ingatlanok/${ing.id}/${item.filename}`;
+                let extIndex = file.originalname.lastIndexOf('.');
+                let fname = file.originalname.substring(0, extIndex);
+                const ref = `${fname}.jpg`;
+
+                item.src = `http://inftechsol.hu:8460/static/images/ingatlanok/${ing.id}/${ref}`;
                 return item;
             });
             return ing;
