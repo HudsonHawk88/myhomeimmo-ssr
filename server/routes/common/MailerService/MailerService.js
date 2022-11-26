49f1b1b6f8c7b1fe96250e64e05999685d67b3e6
HudsonHawk88 <toth.gergo88@hotmail.com>
2022-11-26 22:10:43 +0100

    fix(App): fix emailconfig

--- server/routes/common/MailerService/MailerService.js
+++ server/routes/common/MailerService/MailerService.js
@@ -8,26 +8,19 @@ const router = express.Router();
 
 router.post('/', (req, res) => {
     const emailObj = JSON.parse(JSON.stringify(req.body));
-    // verify connection configuration
-    transporter.verify(function (error, success) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Server is ready to take our messages');
-        }
-    });
+
     transporter.sendMail(
         {
             from: `${emailObj.nev} <${emailObj.email}>`, // sender address
             to: 'berkimonika@myhomezala.hu', // list of receivers
             subject: `${emailObj.statusz + ' ingatlan'}`, // Subject line
             html: `<b>Kedves Berki Mónika!</b><br><br>
-    Az én nevem: ${emailObj.nev}.<br>
-    Telefonszámom: ${emailObj.telefon}.<br><br>
-    ${emailObj.statusz} ingatlanom van! Kérem vegye fel velem a kapcsolatot!<br><br>
-    Tisztelettel:<br>
-    ${emailObj.nev}<br>
-    ` // html body
+            Az én nevem: ${emailObj.nev}.<br>
+            Telefonszámom: ${emailObj.telefon}.<br><br>
+            ${emailObj.statusz} ingatlanom van! Kérem vegye fel velem a kapcsolatot!<br><br>
+            Tisztelettel:<br>
+            ${emailObj.nev}<br>
+            ` // html body
         },
         (err) => {
             if (!err) {
@@ -42,6 +35,14 @@ router.post('/', (req, res) => {
 router.post('/ingatlanerd', (req, res) => {
     const emailObj = req.body;
     console.log(emailObj, emailObj.nev);
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Server is ready to take our messages');
+        }
+    });
     transporter.sendMail(
         {
             from: `${emailObj.email}`, // sender address
