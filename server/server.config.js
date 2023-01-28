477e9d23207f0bc108e4f65aa839b9ab8d5b70aa
HudsonHawk88 <toth.gergo88@hotmail.com>
2023-01-28 23:17:49 +0100

    fix(App): fix email send and rechaptcha

--- server/server.config.js
+++ server/server.config.js
@@ -6,7 +6,7 @@ module.exports = {
         {
             name: 'myhome',
             script: path.resolve(__dirname, 'bundle.js'),
-            node_args: 'NODE_TLS_REJECT_UNAUTHORIZED',
+            node_args: 'NODE_TLS_REJECT_UNAUTHORIZED=0',
             instances: 'max',
             exec_mode: 'cluster'
         }
