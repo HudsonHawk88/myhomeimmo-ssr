1120b66b47358d76455339654a62ad71f95e5402
HudsonHawk88 <toth.gergo88@hotmail.com>
2023-02-13 21:17:40 +0100

    feat(App): designváltás - MYHOME-31

--- src/containers/Public.js
+++ src/containers/Public.js
@@ -47,7 +47,7 @@ const Public = (props) => {
                 )}
                 <PublicFooter />
             </div>
-         
+            
         </React.Fragment>
     );
 };
--- src/views/Public/Fooldal/Fooldal.js
+++ src/views/Public/Fooldal/Fooldal.js
@@ -4,6 +4,7 @@ import { Helmet } from 'react-helmet';
 import FooldalContent from './FooldalContent';
 
 const Fooldal = (props) => {
+
     return (
         <div className="public-inner-content">
             <Helmet>
